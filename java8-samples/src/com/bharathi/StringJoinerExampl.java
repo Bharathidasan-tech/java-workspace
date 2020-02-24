d2c6e0cb6f407be7b3ba19de5f1de9eecdc9efdb
Bharathidasan <mailtobmanickam@gmail.com>
2020-02-24 14:40:14 +0530

    Java 8 Examples: Convert List to Map StringJoiner FuncationalInterface

---
+++ java8-samples/src/com/bharathi/StringJoinerExampl.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package com.bharathi;
+
+import java.util.StringJoiner;
+
+/**
+ * @author manickamb
+ *
+ */
+public class StringJoinerExampl {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		// Passing Hyphen(-) as delimiter
+        StringJoiner l_stringVal = new StringJoiner(" - ");    
+          
+        // Joining multiple strings by using add() method  
+        l_stringVal.add("Rock");  
+        l_stringVal.add("Bharathi");  
+        l_stringVal.add("Dasan");  
+        l_stringVal.add("Bharathidasan");  
+                  
+        // output String
+        System.out.println(l_stringVal);  
+        
+        StringJoiner prefix_suffix   = new StringJoiner(",", "{", "}"); 
+        
+        prefix_suffix.add("Rock");  
+        prefix_suffix.add("Bharathi");  
+        prefix_suffix.add("Dasan");  
+        prefix_suffix.add("Bharathidasan"); 
+        
+        // output String
+        System.out.println(prefix_suffix);  
+        
+        StringJoiner mergedStringval = l_stringVal.merge(prefix_suffix);   
+        
+    	System.out.println(mergedStringval);  
+        
+	}
+
+}
